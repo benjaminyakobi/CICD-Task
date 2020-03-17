c04b7325685fb7547f512f43a5e7f1ca20857f05
kostyve@ac.sce.ac.il <kostyve@ac.sce.ac.il>
2020-03-17 15:05:04 +0200

    added a test and added baseline for functionality for the app

--- src/CardsDeck.py
+++ src/CardsDeck.py
@@ -1,11 +1,15 @@
 import requests
 import webbrowser
 
+# get a new unshuffled deck with
+URL_NEW_DECK_UNSHUFFLED = 'https://deckofcardsapi.com/api/deck/new/'
+URL_NEW_DECK_SHUFFLED = 'https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1'
+URL_DRAW_CARDS = 'https://deckofcardsapi.com/api/deck/<<deck_id>>/draw/?count='
 class CardsDeck:
 
     @staticmethod
     def get_new_unshuffled_deck():
-        api_result = requests.get('https://deckofcardsapi.com/api/deck/new/')
+        api_result = requests.get(URL_NEW_DECK_UNSHUFFLED)
         api_response = api_result.json()
         print('Remaining cards: ' + str(api_response['remaining']) + ', '
               + 'Shuffled: ' + str(api_response['shuffled']) + ', '
@@ -14,7 +18,7 @@ class CardsDeck:
 
     @staticmethod
     def get_new_shuffled_deck():
-        api_result = requests.get('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')
+        api_result = requests.get(URL_NEW_DECK_SHUFFLED)
         api_response = api_result.json()
         print('Remaining cards: ' + str(api_response['remaining']) + ', '
               + 'Shuffled: ' + str(api_response['shuffled']) + ', '
@@ -22,9 +26,17 @@ class CardsDeck:
         return api_response
 
     @staticmethod
-    def draw_cards():
+    def draw_cards(deck_id=None):
+        # deck_id will be used to draw cards from the same deck
         num = input('Enter number: ')
-        api_result = requests.get('https://deckofcardsapi.com/api/deck/new/draw/?count=' + str(num))
+        # if deck_id exists->deck already created ->use the existing deck and deraw from it,\
+        # otherwise create new deck and draw
+        if deck_id:
+            api_result = \
+                requests.get('https://deckofcardsapi.com/api/deck/<<deck_id>>/draw/?count='
+                             .replace('<<deck_id>>', deck_id) + str(num))
+        else:
+            api_result = requests.get('https://deckofcardsapi.com/api/deck/new/draw/?count=' + str(num))
         api_response = api_result.json()
         for i in range(0, len(api_response['cards'])):
             print(api_response['cards'][i]['code'] + ', '
@@ -34,12 +46,12 @@ class CardsDeck:
         return api_response
 
     @staticmethod
-    def get_cards(type):
-        if(type == 1):
+    def get_cards(select):
+        if select == 1:
             CardsDeck.get_new_unshuffled_deck()
 
-        if(type == 2):
+        if select == 2:
             CardsDeck.get_new_shuffled_deck()
 
-        if(type == 3):
+        if select == 3:
             CardsDeck.draw_cards()
--- test/CardsDeck_test.py
+++ test/CardsDeck_test.py
@@ -3,29 +3,34 @@ from unittest.mock import Mock, patch
 from src.CardsDeck import CardsDeck
 
 class CardsDeckTest(unittest.TestCase):
+
+    def test_calling_api(self):
+        pass
+
     @patch('src.CardsDeck.CardsDeck.get_new_unshuffled_deck')
     def test_get_new_unshuffled_card_deck(self, mock_get_new_unshuffled_card_deck):
-        #simulated deck to test if the deck is correctly created
+        # simulated deck to test if the deck is correctly created
         new_deck = {
             "success": True,
             "deck_id": "111111111111",
             "shuffled": False,
             "remaining": 52
         }
-        #mock init
+        # mock init
         mock_get_new_unshuffled_card_deck.return_value.status_code = 200
         mock_get_new_unshuffled_card_deck.return_value.json.return_value = new_deck
-        #expected creating new deck, status code 200 and deck success == True
+        # expected creating new deck, status code 200 and deck success == True
         n = CardsDeck()
         response = n.get_new_unshuffled_deck()
-        #assert
+        # assert
         assert response.status_code == 200
-        #success of creation
+        # success of creation
         self.assertEqual(response.json()['success'], True)
-        #success when cards == 52
+        # success when cards == 52
         self.assertEqual(response.json()['remaining'], 52)
 
     def test_get_new_shuffled_deck(self):
+        # mock the card deck
         new_ush_deck = Mock(CardsDeck)
         new_ush_deck.get_new_unshuffled_deck.json.return_value = \
         {
@@ -34,9 +39,56 @@ class CardsDeckTest(unittest.TestCase):
             "shuffled": True,
             "remaining": 52
         }
+        # assume deck created as intended
+        cards_remaining = 52
+        is_shuffled = True
 
-        self.assertEqual(new_ush_deck.get_new_unshuffled_deck.json.return_value['remaining'], 52)
-        self.assertEqual(new_ush_deck.get_new_unshuffled_deck.json.return_value['shuffled'], True)
+        # assert
+        self.assertEqual(new_ush_deck.get_new_unshuffled_deck.json.return_value['remaining'], cards_remaining)
+        self.assertEqual(new_ush_deck.get_new_unshuffled_deck.json.return_value['shuffled'], is_shuffled)
+
+    @patch('src.CardsDeck.CardsDeck.draw_cards')
+    def test_draws_card_new_deck_returns_two(self, mock_draw_cards):
+        # simulated deck to test if the deck is correctly created
+        new_deck = {
+                    "success": True,
+                    "cards": [
+                        {
+                            "image": "https://deckofcardsapi.com/static/img/KH.png",
+                            "value": "KING",
+                            "suit": "HEARTS",
+                            "code": "KH"
+                        },
+                        {
+                            "image": "https://deckofcardsapi.com/static/img/8C.png",
+                            "value": "8",
+                            "suit": "CLUBS",
+                            "code": "8C"
+                        }
+                    ],
+                    "deck_id":"3p40paa87x90",
+                    "remaining": 50
+        }
+        mock_draw_cards.return_value.status_code = 200
+        mock_draw_cards.return_value.json.return_value = new_deck
+
+        n = CardsDeck()
+        response = n.draw_cards()
+
+        assert response.status_code == 200
+        # mock init
+
+        cards_left_in_deck = 50
+        size_of_cards = 2
+
+        self.assertEqual(response.json()['remaining'], cards_left_in_deck)
+        #summation of each card returned, expected 2
+        self.assertEqual(sum(list(1 for x in response.json()['cards'])), size_of_cards)
+
+
+class DeckHash(unittest.TestCase):
+    pass
+
 
 if __name__ == '__main__':
     unittest.main()
